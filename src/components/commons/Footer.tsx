ff6fe611b27495eebc542856a8dac76067ca2b5b
seonga <98681659+hongseonga@users.noreply.github.com>
2022-12-20 16:18:23 +0900

    markup: Footer markup

--- src/components/commons/Footer.tsx
+++ src/components/commons/Footer.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import styled from "styled-components";
+import Logo from "../../assets/Logo.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <MainWrapper>
+      <FooterBox>
+        <LogoImage src={Logo} alt="logo-img" />
+        <FontAwesomeIcon className="github" icon={faGithub} size="4x" />
+      </FooterBox>
+    </MainWrapper>
+  );
 };
 
 export default Footer;
+
+const MainWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 300px;
+  background-color: var(--black-030);
+  margin-top: 200px;
+  position: sticky;
+  z-index: 10000;
+`;
+
+const FooterBox = styled.div`
+  width: 1300px;
+  .github {
+    color: #fff;
+  }
+`;
+
+const LogoImage = styled.img`
+  width: 90px;
+`;
